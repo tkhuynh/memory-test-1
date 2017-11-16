5369dd4f0d938fb1286c1ed8aff191a493172181
tkhuynh <tkhuynh2311@gmail.com>
2017-11-16 14:57:43 -0800

    typo

--- app/controllers/kid_controller.rb
+++ app/controllers/kid_controller.rb
@@ -3,7 +3,7 @@ class KidController < ApplicationController
   def create
   	states = States.get
   	kid = {name: params[:name],email: params[:email],state: states[params[:state].to_s.to_sym]}
-  	found_kid = kid.where(email: params[:email]).first
+  	found_kid = Kid.where(email: params[:email]).first
   	if found_kid
   		render json: { message: 'kid exist'}
   	else
