c3bc3260cf9841fa836b2ffb8528e8bc728e8703
tkhuynh <tkhuynh2311@gmail.com>
2017-11-16 18:28:03 -0800

    testing pool

--- app/controllers/kid_controller.rb
+++ app/controllers/kid_controller.rb
@@ -15,4 +15,42 @@ class KidController < ApplicationController
 	  end
 	  report.pretty_print
   end
+
+
+  def continuously
+  	pool = Concurrent::CachedThreadPool.new
+  	pool.post do
+  		sleep 1
+  		puts "#{__method__} - TOTAL THREADS BEFORE SHUTDOWN from QUERY #{params[:query_number]}: \e[35m#{Thread.list.length}\e[0m"
+  		states = States.get
+	  	kid = {name: params[:name],email: params[:email],state: states[params[:state].to_s.to_sym]}
+	  	found_kid = Kid.where(email: params[:email]).first
+	  	if found_kid
+	  		render json: { message: 'kid exist'}
+	  	else
+	  		created_kid = Kid.create(kid)
+	  		render json: kid
+	  	end
+  	end
+  	pool.shutdown
+  	puts "#{__method__} - TOTAL THREADS AFTER SHUTDOWN from QUERY #{params[:query_number]}: \e[35m#{Thread.list.length}\e[0m"
+  end
+
+  def continuously_no_pools
+  	Thread.new do
+  		sleep 1
+  		puts "#{__method__} - TOTAL THREADS BEFORE SHUTDOWN from QUERY #{params[:query_number]}: \e[35m#{Thread.list.length}\e[0m"
+  		states = States.get
+	  	kid = {name: params[:name],email: params[:email],state: states[params[:state].to_s.to_sym]}
+	  	found_kid = Kid.where(email: params[:email]).first
+	  	if found_kid
+	  		render json: { message: 'kid exist'}
+	  	else
+	  		created_kid = Kid.create(kid)
+	  		render json: kid
+	  	end
+  	end
+  	puts "#{__method__} - TOTAL THREADS AFTER SHUTDOWN from QUERY #{params[:query_number]}: \e[35m#{Thread.list.length}\e[0m"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   post 'customer/create', to: "customer#create"
   post 'kid/create', to: "kid#create"
+  post 'continuously', to: "kid#continuously"
+  post 'continuously_no_pools', to: "kid#continuously_no_pools"
 end
