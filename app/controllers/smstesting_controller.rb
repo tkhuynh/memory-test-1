7f00221f7edb321fd9c74ec8ed051392331e5eb8
tkhuynh <tkhuynh2311@gmail.com>
2017-12-12 18:57:22 -0800

    render text for sms test

--- app/controllers/smstesting_controller.rb
+++ app/controllers/smstesting_controller.rb
@@ -3,19 +3,20 @@ class SmstestingController < ApplicationController
 	
 	def dummy
 		rand_num = rand().to_s[-9..-1]
-    render json: {"keys": {
-      MessageID: rand_num
-     },
-     "values": {
-       MessageName: "Testing SFMC",
-       MessageQueuedStatus: "ET:0201",
-       MessageQueuedStatusCode: "200",
-       MessageContent: "ABCDEFG",
-       PhoneNumber: "123456789",
-       MessageID: rand_num,
-       MessageDeliveryStatus: "DEL",
-       MessageDeliveryStatusCode: "200"
-      }
-    }
+    # render json: {"keys": {
+    #   MessageID: rand_num
+    #  },
+    #  "values": {
+    #    MessageName: "Testing SFMC",
+    #    MessageQueuedStatus: "ET:0201",
+    #    MessageQueuedStatusCode: "200",
+    #    MessageContent: "ABCDEFG",
+    #    PhoneNumber: "123456789",
+    #    MessageID: rand_num,
+    #    MessageDeliveryStatus: "DEL",
+    #    MessageDeliveryStatusCode: "200"
+    #   }
+    # }
+    render text: "command=MT_RESPONSE&spid=100941&mtmsgid=#{rand_num}&mtstat=ACCEPTD&mterrcode=000"
 	end
 end
