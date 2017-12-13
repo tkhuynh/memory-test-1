eae2c7fc23387f28c26ae6152c5b0515d8fc55f2
tkhuynh <tkhuynh2311@gmail.com>
2017-12-13 15:56:58 -0800

    50 ms

--- app/controllers/smstesting_controller.rb
+++ app/controllers/smstesting_controller.rb
@@ -5,7 +5,7 @@ class SmstestingController < ApplicationController
     rand_num = rand().to_s[-9..-1]
     url = "https://pierrysmsdev.herokuapp.com/smslogs?command=RT_REQUEST&spid=100941&mtmsgid=#{rand_num}&mttotal=3&mtseq=1&mtstat=DELIVRD&mterrcode=000"
     Thread.new do
-      sleep 0.1
+      sleep 0.050
       HTTParty.get(url)
     end
     render text: "command=MT_RESPONSE&spid=100941&mtmsgid=#{rand_num}&mtstat=ACCEPTD&mterrcode=000"
