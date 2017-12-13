a1bfaec6e6a9ca11c823d770f2850ab60b6381a6
tkhuynh <tkhuynh2311@gmail.com>
2017-12-13 15:28:17 -0800

    get request to smslogs

--- app/controllers/smstesting_controller.rb
+++ app/controllers/smstesting_controller.rb
@@ -1,22 +1,13 @@
 class SmstestingController < ApplicationController
-	skip_before_action :verify_authenticity_token
-	
-	def dummy
-		rand_num = rand().to_s[-9..-1]
-    # render json: {"keys": {
-    #   MessageID: rand_num
-    #  },
-    #  "values": {
-    #    MessageName: "Testing SFMC",
-    #    MessageQueuedStatus: "ET:0201",
-    #    MessageQueuedStatusCode: "200",
-    #    MessageContent: "ABCDEFG",
-    #    PhoneNumber: "123456789",
-    #    MessageID: rand_num,
-    #    MessageDeliveryStatus: "DEL",
-    #    MessageDeliveryStatusCode: "200"
-    #   }
-    # }
+  skip_before_action :verify_authenticity_token
+  include HTTParty
+  def dummy
+    rand_num = rand().to_s[-9..-1]
+    url = "https://pierrysmsdev.herokuapp.com/smslogs?command=RT_REQUEST&spid=100941&mtmsgid=#{rand_num}&mttotal=3&mtseq=1&mtstat=DELIVRD&mterrcode=000"
+    Thread.new do
+      sleep 0.1
+      HTTParty.get(url)
+    end
     render text: "command=MT_RESPONSE&spid=100941&mtmsgid=#{rand_num}&mtstat=ACCEPTD&mterrcode=000"
-	end
+  end
 end
