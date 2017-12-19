0ffce344a34362612bd60743238cd17f670740eb
tkhuynh <tkhuynh2311@gmail.com>
2017-12-18 17:20:29 -0800

    new url testing

--- app/controllers/smstesting_controller.rb
+++ app/controllers/smstesting_controller.rb
@@ -3,11 +3,11 @@ class SmstestingController < ApplicationController
   include HTTParty
   def dummy
     rand_num = rand().to_s[-9..-1]
-    # url = "https://pierrysmsdev.herokuapp.com/smslogs?command=RT_REQUEST&spid=100941&mtmsgid=#{rand_num}&mttotal=3&mtseq=1&mtstat=DELIVRD&mterrcode=000"
-    # Thread.new do
-    #   sleep 0.2
-    #   HTTParty.get(url)
-    # end
+    url = "https://esprit-sendreport.herokuapp.com/smslogs?command=RT_REQUEST&spid=100941&mtmsgid=#{rand_num}&mttotal=3&mtseq=1&mtstat=DELIVRD&mterrcode=000"
+    Thread.new do
+      sleep 0.2
+      HTTParty.get(url)
+    end
     render text: "command=MT_RESPONSE&spid=100941&mtmsgid=#{rand_num}&mtstat=ACCEPTD&mterrcode=000"
   end
 end
